1019a9751ba33656b5d6927226e24fd405dd283f
du4 <shkantau@gmail.com>
2025-09-29 15:30:37 +0400

    Use TIM4,2 to manage UART TX data sending.

--- Core/Src/stm32h7xx_it.c
+++ Core/Src/stm32h7xx_it.c
@@ -249,9 +249,9 @@ void TIM4_IRQHandler(void)
 		HAL_TIM_Base_Stop_IT(&htim4);
 //		cutIdTx = 1001;
 	}else{
-		ethPressuresBankFullStatus = SET;
-		cutIdTx += FIRST_SECTION_CUTS_PER_PACKET;
-//		HAL_UART_Transmit_DMA(&huart4, (uint8_t*)pFirstSectionPacketTX, FIRST_SECTION_CUTS_PER_PACKET*SECTION_PACKET_SIZE);
+//		ethPressuresBankFullStatus = SET;
+//		cutIdTx += FIRST_SECTION_CUTS_PER_PACKET;
+		HAL_UART_Transmit_DMA(&huart4, (uint8_t*)pFirstSectionPacketTX, FIRST_SECTION_CUTS_PER_PACKET*SECTION_PACKET_SIZE);
 	}
   /* USER CODE END TIM4_IRQn 0 */
   HAL_TIM_IRQHandler(&htim4);
